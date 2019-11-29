19f5ec7ff8e01eb6e36101cdcb55bdbe3d8f9b4f
MuttakinHasib <hasibmolla28@gmail.com>
2019-11-30 00:34:21 +0600

    added preloader and much more

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,13 @@
+$(window).on('load', function () {
+    $('.preloader').fadeOut(500);
+});
 // ! SCROLL TO ACTIVE NAVBAR
 
 $(window).scroll(function () {
     var winScroll = $(this).scrollTop();
     if (winScroll >= 110) {
         $('.main-menu').addClass('header-active');
-    }else{
+    } else {
         $('.main-menu').removeClass('header-active');
     }
 });
