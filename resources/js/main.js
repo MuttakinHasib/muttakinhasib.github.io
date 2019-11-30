63d05a396ed915e96ca222b5d8b44079f75d4f94
MuttakinHasib <hasibmolla28@gmail.com>
2019-11-30 19:45:13 +0600

    added testimonial section

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,44 @@
 // ! PRELOADER
-$(window).on('load', function () {
-    $('.block').delay(450).fadeOut(1000);
-    $('.preloader').delay(500).fadeOut(500);
+$(window).on("load", function() {
+    $(".block")
+        .delay(450)
+        .fadeOut(1000);
+    $(".preloader")
+        .delay(500)
+        .fadeOut(500);
 });
 
 // ! SCROLL TO ACTIVE NAVBAR
 
-$(window).scroll(function () {
+$(window).scroll(function() {
     var winScroll = $(this).scrollTop();
     if (winScroll >= 110) {
-        $('.main-menu').addClass('header-active');
+        $(".main-menu").addClass("header-active");
     } else {
-        $('.main-menu').removeClass('header-active');
+        $(".main-menu").removeClass("header-active");
     }
+});
+
+$(document).ready(function() {
+    //Testimonial Slider
+
+    $(".testimonial-slider").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        // nav: true,
+        autoplay: true,
+        mouseDrag: true,
+        responsive: {
+            1280: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
 });
