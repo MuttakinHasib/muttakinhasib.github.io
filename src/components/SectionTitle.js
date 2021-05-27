a20d9644bc883d90254fa588d2b8831488b9c9cf
MuttakinHasib <hasibmolla28@gmail.com>
2021-05-27 19:24:58 +0600

    :hammer:

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -4,7 +4,7 @@ const SectionTitle = ({ index, title }) => {
   return (
     <div className='flex items-center space-x-4 mt-5 mb-10'>
       <p className='text-[20px] text-green font-SFMono'>{index}</p>
-      <h1 className='relative font-SFMono font-semibold text-[27px] whitespace-nowrap'>
+      <h1 className='relative font-semibold text-[27px] whitespace-nowrap'>
         {title}
       </h1>
       <span className='w-52 h-[1px] bg-[#233554]' />
--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -8,7 +8,7 @@ const About = () => {
       <div className='grid gap-50 col-1'>
         <div className='col-span-2'>
           <div>
-            <p className='max-w-[500px] ml-1 mt-5 prose prose-lg text-gray-300'>
+            <p className='max-w-[500px] ml-1 mt-5 prose prose-lg text-lightSlate'>
               A passionate self-taught Full Stack developer from Bangladesh.
               <br />I develop web applications, mobile applications and desktop
               applications
