8c81780b7e8c98dd669e05521775f1a244d80c11
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-02 00:55:00 +0600

    contact component

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -2,3 +2,4 @@ export { default as Landing } from './Landing';
 export { default as About } from './About';
 export { default as Experience } from './Experience';
 export { default as Work } from './Work';
+export { default as Contact } from './Contact';
