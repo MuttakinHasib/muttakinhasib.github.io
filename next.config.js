326a7a933dc01012a43c89f428ce95406a302dd3
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-02 12:49:49 +0600

    :bug:

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-const withOptimizedImages = require('next-optimized-images');
-
-module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
-  // your config for other plugins or the general next.js here...
-});
