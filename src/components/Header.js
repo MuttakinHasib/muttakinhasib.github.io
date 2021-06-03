910645eed8edd9e8056e12aae2e76f3cd0e1ef13
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-03 12:38:16 +0600

    :zap:

--- src/components/Header.js
+++ src/components/Header.js
@@ -96,7 +96,7 @@ const Header = () => {
         >
           <svg
             xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6'
+            className='h-8 w-8'
             fill='none'
             viewBox='0 0 24 24'
             stroke='currentColor'
