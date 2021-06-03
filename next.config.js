e29db378cf008507e7edcba50eda1cdf02b5f546
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-03 12:32:00 +0600

    added mobile navigation :zap:

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// const withPlugins = require('next-compose-plugins');
-// const nextImg = require('next-img/plugin');
-
-// module.exports = withPlugins([
-//   [
-//     nextImg,
-//     {
-//       // specify the default breakpoints
-//       breakpoints: [768],
-//     },
-//   ],
-// ]);
-
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { createRef, useEffect, useRef, useState } from 'react';
+import { useMediaQuery } from '@react-hook/media-query';
 import Link from 'next/link';
+import { TweenMax, TimelineMax, gsap, Power3 } from 'gsap';
+import { Transition } from '@headlessui/react';
 import useScrollPosition from '../hooks/useScrollPosition';
 import { navLinks } from '../static';
 
 const Header = () => {
   const scrollPosition = useScrollPosition();
+  const isMediumDevice = useMediaQuery('only screen and (min-width: 768px)');
+
+  const [isOpen, setIsOpen] = useState(isMediumDevice);
 
   return (
     <header
-      className={`z-50 bg-darkNavy absolute flex items-center w-full px-6 md:px-10 ${
+      className={`z-50 h-auto backdrop-filter backdrop-blur bg-darkNavy absolute flex items-center w-full px-6 md:py-0 md:px-10 ${
         scrollPosition >= 120
-          ? 'header-active h-[70px] bg-opacity-95'
-          : 'h-[100px]'
+          ? 'header-active py-4 md:h-[70px] bg-opacity-95'
+          : 'md:h-[100px] py-5'
       }`}
     >
-      <nav className='flex items-center justify-between w-full'>
-        <div className='w-[42px] h-[42px]'>
+      <nav className='flex flex-col md:flex-row md:items-center justify-center md:justify-between w-full relative'>
+        <div className='w-[32px] md:w-[42px] w-[32px] md:h-[42px]'>
           <Link href='/'>
             <a className='text-green w-full h-full hover:bg-greenTint transition-colors duration-300 ease-transition'>
               {/* Hasib */}
@@ -43,28 +49,66 @@ const Header = () => {
             </a>
           </Link>
         </div>
-        <div className='hidden md:flex items-center'>
-          <ol className='flex items-center justify-between list-outside'>
+        <Transition
+          appear={true}
+          show={isOpen}
+          enter='transition-opacity transition-[height] duration-500 ease-transition'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='transition-opacity duration-500 ease-transition'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+          className='flex flex-col md:flex-row md:items-center mt-5 md:mt-0'
+        >
+          <ol className='flex flex-col md:flex-row md:items-center md:justify-between list-outside'>
             {navLinks.map((link, index) => (
-              <li
+              <Transition.Child
                 key={index}
-                className='mx-1 relative order font-SFMono text-[13px] leading-[1] text-lightestSlate hover:text-green transition-colors duration-300'
+                as='li'
+                enter='transition-opacity delay-600 transition-transform duration-500 ease-transition'
+                enterFrom='opacity-0'
+                enterTo='opacity-100'
+                leave='transition-opacity delay-600 transition-transform duration-500 ease-transition'
+                leaveFrom='opacity-100'
+                leaveTo='opacity-0'
+                className='md:mx-1 py-3 md:py-0 relative order font-SFMono text-[13px] leading-[1] text-lightestSlate hover:text-green transition-colors duration-300'
               >
                 <a className='p-[10px] order-item' href={link.url}>
                   {link.name}
                 </a>
-              </li>
+              </Transition.Child>
             ))}
           </ol>
-          <a
-            href='/resume.pdf'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-green leading-[1] text-[13px] ml-[15px] font-SFMono font-medium bg-transparent border-2 border-green rounded-[4px] hover:bg-greenTint py-[0.6rem] px-4 transition-colors duration-300 ease-transition'
+          <div>
+            <a
+              href='/resume.pdf'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green inline-block leading-[1] text-[13px] mt-[15px] md:mt-0 ml-[12px] md:ml-[15px] font-SFMono font-medium bg-transparent border-2 border-green rounded-[4px] hover:bg-greenTint py-[0.6rem] px-4 transition-colors duration-300 ease-transition'
+            >
+              Resume
+            </a>
+          </div>
+        </Transition>
+        <button
+          className='inline-block md:hidden focus:outline-none text-green absolute top-0 right-0'
+          onClick={() => setIsOpen(prev => !prev)}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
           >
-            Resume
-          </a>
-        </div>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
       </nav>
     </header>
   );
