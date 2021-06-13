9c37595e432b175e439797ce12908386f6e1337e
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-13 18:40:53 +0000

    Deploying to gh-pages from @ MuttakinHasib/muttakinhasib.github.io@725b827bc114e809f783e872f29963cc5c099bc3 🚀

---
+++ _next/static/2AyzQK5uFVH0sVpeIx4fH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-97d1140319a187769e66.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fc1a5bab6b411a9de7de.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
