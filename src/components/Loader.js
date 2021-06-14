edd09db26390687b993db0803175abed6a4ef70b
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-14 14:32:32 +0600

    :hammer:

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -51,9 +51,9 @@ const Loader = ({ setLoading }) => {
     <AnimatePresence>
       <motion.div
         key='loader'
-        className='bg-darkNavy w-screen h-screen flex items-center justify-center fixed inset-0 z-[99]'
+        className='bg-darkNavy w-screen h-screen flex items-center justify-center relative z-[99]'
       >
-        <div className='w-20 h-20 text-green'>
+        <div className='w-48 h-48 text-green'>
           <motion.svg
             variants={svgVariants}
             initial='hidden'
@@ -62,22 +62,39 @@ const Loader = ({ setLoading }) => {
             id='logo'
             xmlns='http://www.w3.org/2000/svg'
             role='img'
-            viewBox='0 0 84 96'
+            viewBox='0 0 0 0 100 100'
             fill='none'
           >
             <title>Logo</title>
-            <motion.path
+            {/* <motion.path
               variants={pathVariants}
               d='M42 3L3 25v45l39 23 39-22V26z'
               fill='none'
               stroke='currentColor'
               strokeWidth={5}
+            /> */}
+            <motion.path
+              variants={pathVariants}
+              stroke='currentColor'
+              strokeWidth='5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z'
             />
             <g>
               <motion.path
                 variants={textVariants}
+                id='eEOMqLXQADV4'
+                d='M29.300000,63.900000L29.300000,32.100000L35.700000,32.100000L35.700000,44.600000L48.300000,44.600000L48.300000,32.100000L54.700000,32.100000L54.700000,63.900000L48.300000,63.900000L48.300000,50L35.700000,50L35.700000,63.900000L29.300000,63.900000Z'
+                transform='matrix(1 0 0 1 8 2)'
                 fill='currentColor'
-                d='M29.3,63.9V32.1h6.4v12.5h12.6V32.1h6.4v31.8h-6.4V50H35.7v13.9H29.3z'
+                stroke='none'
+                stroke-width='5'
               />
             </g>
           </motion.svg>
